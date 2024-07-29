c68e4028cda2e7b82124aeee014055d9feb3ee91
DilanNuma <dilanmatheonuma@gmail.com>
2024-07-29 14:09:12 -0500

    add feature models

---
+++ src/models/fileController.ts
@@ -0,0 +1,32 @@
+import { DataRow,DataTable,ColumName  } from "./models.js";
+
+export class FileController{
+    private data: DataTable = [];
+    private columNames: ColumName = [];
+
+    constructor(private fileContent:string){
+        this.processFile();
+    }
+
+    private processFile(){
+        const lines = this.fileContent.split(/[\r\n]+/).filter(line=>line.trim()!=='')//salto de linea
+        if(lines.length > 0){
+            this.columNames=lines[0].split(',')
+            this.data =  lines.slice(1).map(lines=>{
+                const values = lines.split(',');
+                const row: DataRow = {};
+                this.columNames.forEach((colName,index)=>{
+                    row[colName] = values[index] || ''
+                })
+                return row
+            });
+            
+        }
+    }
+    getData():DataTable{
+        return this.data
+    }
+    getColumNames(): ColumName {
+        return this.columNames
+    }
+}
---
+++ src/models/models.ts
@@ -0,0 +1,7 @@
+export interface DataRow{
+    [key:string]:string //arreglo de string
+}
+
+export type DataTable = DataRow[]
+
+export type ColumName = string[];//arreglo de eestilos para hacer las filas y columnas (tablas)
